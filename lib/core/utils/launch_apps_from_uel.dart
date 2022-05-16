c5777ec20445aa8cb117a9dc1b74327533faac39
MonaGamal2019 <mona@megatrust.co>
2022-05-16 18:13:16 +0200

    [ADD] url lunacher for mail

--- lib/core/utils/launch_apps_from_uel.dart
+++ lib/core/utils/launch_apps_from_uel.dart
@@ -2,9 +2,11 @@ import 'dart:developer';
 
 import 'package:url_launcher/url_launcher.dart';
 
-void openMail() {
-  canLaunchUrl(Uri(scheme: 'mailto:')).then(
-    (bool result) {
+void openMail() async {
+  final url = Uri(scheme: 'mailto');
+  canLaunchUrl(url).then(
+    (bool result) async {
+      await launchUrl(url);
       log("$result can launch");
     },
   );
